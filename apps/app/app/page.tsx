3e0dcb595a9ae8feb9c3a201702e2937c19c061c
Joey <jx1911@gmail.com>
2023-07-13 16:56:48 -0400

    Renamed index.tsx to correct extension of ts

--- apps/app/app/page.tsx
+++ apps/app/app/page.tsx
@@ -3,8 +3,15 @@ import {
   Container
 } from '@allowance/bash-ui';
 import LoginForm from '@/components/LoginForm';
+import { Client } from '@allowance/slade';
 
-export default function Page() {
+export default async function Page() {
+  const ApiClient = new Client(process.env.NEXT_MARQETA_API_URL, process.env.NEXT_MARQETA_APPLICATION_TOKEN, process.env.NEXT_MARQETA_ACCESS_TOKEN);
+  console.log(ApiClient);
+  /*
+  const results = ApiClient.makeRequest('cardproducts?sort_by=-lastModifiedTime', 'get');
+  console.log(results);
+  */
   return (
     <>
       <Section type="body">
