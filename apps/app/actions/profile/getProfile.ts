3efa75f9370d330cb2542f3cf1bfb2f12c77652a
Joey <jx1911@gmail.com>
2023-07-14 10:41:10 -0400

    Refactor "get profile" to follow standard practices

--- apps/app/actions/profile/getProfile.ts
+++ apps/app/actions/profile/getProfile.ts
@@ -1,10 +1,6 @@
 import { cookies } from 'next/headers';
 import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
 import type { Database } from '@/types/supabase';
-import {
-  type AccountProfileRow,
-  type AccountProfile
-} from '@/types/account'; // eslint-disable-line import/no-unresolved
 
 /**
  * Get user's profile data
@@ -12,26 +8,20 @@ import {
  * @returns {object} profileData - Current profile data
  * @returns {boolean} false - If user isn't found, return false
  */
-export default async function getProfile(userId: string): Promise<AccountProfile> {
+export default async function getProfile(userId: string) {
   const supabase = createServerComponentClient<Database>({ cookies });
 
-  const {
-    data,
-    error,
-    status
-  }: {
-    data: AccountProfileRow,
-    error: any,
-    status: any
-  } = await supabase
-    .from('profiles')
-    .select(`first_name, last_name, address, address2, city, state, zip_code, dob, avatar_url`)
-    .eq('id', userId)
-    .single();
-
-  if (error && status !== 406) {
-    throw error;
+  try {
+    return await supabase
+      .from('profiles')
+      .select(`first_name, last_name, address, address2, city, state, zip_code, dob, avatar_url`)
+      .eq('id', userId)
+      .single();
+  } catch (error) {
+    return error;
   }
-
-  return data;
 };
+
+type GetProfileResponse = Awaited<ReturnType<typeof getProfile>>
+export type GetProfileResponseSuccess = GetProfileResponse['data']
+export type GetProfileResponseError = GetProfileResponse['error']
--- apps/app/components/profile/ProfileFormContainer.tsx
+++ apps/app/components/profile/ProfileFormContainer.tsx
@@ -1,8 +1,10 @@
 import { redirect } from 'next/navigation';
 import type { Session } from '@supabase/auth-helpers-nextjs';
 import getSession from '@/actions/getSession'; // eslint-disable-line import/no-unresolved
-import getProfile from '@/actions/profile/getProfile'; // eslint-disable-line import/no-unresolved
-import type { AccountProfile } from '@/types/account';
+import getProfile, {
+  type GetProfileResponseSuccess,
+  type GetProfileResponseError
+} from '@/actions/profile/getProfile'; // eslint-disable-line import/no-unresolved
 import ProfileForm from './ProfileForm';
 
 const ProfileFormContainer = async () => {
@@ -12,29 +14,32 @@ const ProfileFormContainer = async () => {
     redirect('/');
   }
 
-  try {
-    const data: AccountProfile = await getProfile(session.user.id);
-    // Default missing values to '' so React Hook Form doesn't complain
-    const profileData = {
-      first_name: data?.first_name ?? '',
-      last_name: data?.last_name ?? '',
-      address: data?.address ?? '',
-      address2: data?.address2 ?? '',
-      city: data?.city ?? '',
-      state: data?.state ?? '',
-      zip_code: data?.zip_code ?? '',
-      dob: data?.dob ?? '',
-      avatar_url: data?.avatar_url ?? ''
-    };
+  const {
+    data,
+    error
+  }: {
+    data: GetProfileResponseSuccess,
+    error: GetProfileResponseError
+  } = await getProfile(session.user.id);
 
-    return <ProfileForm profileData={profileData} user={session.user} />
-  } catch (error) {
-    // record error
+  if (error) {
     return <div>{error.message}</div>
-  } finally {
-    // log access attempts potentially?
-    // this block will execute regardless of the try result
   }
+
+  // Default missing values to '' so React Hook Form doesn't complain
+  const profileData = {
+    first_name: data?.first_name ?? '',
+    last_name: data?.last_name ?? '',
+    address: data?.address ?? '',
+    address2: data?.address2 ?? '',
+    city: data?.city ?? '',
+    state: data?.state ?? '',
+    zip_code: data?.zip_code ?? '',
+    dob: data?.dob ?? '',
+    avatar_url: data?.avatar_url ?? ''
+  };
+
+  return <ProfileForm profileData={profileData} user={session.user} />
 };
 
 export default ProfileFormContainer;
