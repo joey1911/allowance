bbebdb04f15f0b2efe11f56073b6327db09ef57a
Joey <jx1911@gmail.com>
2023-07-17 19:36:21 -0400

    Fixed Forgot Password flow

--- apps/app/app/api/auth/callback/route.ts
+++ apps/app/app/api/auth/callback/route.ts
@@ -9,11 +9,12 @@ import type { Database } from '@/types/supabase';
 export async function GET(request: NextRequest) {
   const requestUrl = new URL(request.url);
   const code = requestUrl.searchParams.get('code');
+  const nextUrl = requestUrl.searchParams.get('next');
 
   if (code) {
     const supabase = createRouteHandlerClient<Database>({ cookies });
     await supabase.auth.exchangeCodeForSession(code);
   }
 
-  return NextResponse.redirect(requestUrl.origin);
+  return NextResponse.redirect(nextUrl ? new URL(nextUrl, request.url) : requestUrl.origin);
 };
